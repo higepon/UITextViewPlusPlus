354140a7fd49ceb048252103192f3874b874249d
Taro Minowa Higepon <higepon@gmail.com>
2014-04-20 11:22:55 -0700

    Added empty class :)

---
+++ UITextViewPlusPlus/UITextViewPlusPlus.h
@@ -0,0 +1,12 @@
+//
+// Created by Taro Minowa on 4/20/14.
+// Copyright (c) 2014 Higepon Taro Minowa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UITextViewPlusPlus : UITextView
+
+- (instancetype)initWithFrame:(CGRect)frame textContainer:(NSTextContainer *)textContainer;
+
+@end
---
+++ UITextViewPlusPlus/UITextViewPlusPlus.m
@@ -0,0 +1,21 @@
+//
+// Created by Taro Minowa on 4/20/14.
+// Copyright (c) 2014 Higepon Taro Minowa. All rights reserved.
+//
+
+#import "UITextViewPlusPlus.h"
+
+
+@implementation UITextViewPlusPlus {
+
+}
+
+- (instancetype)initWithFrame:(CGRect)frame textContainer:(NSTextContainer *)textContainer
+{
+    self = [super initWithFrame:frame textContainer:textContainer];
+    if (self) {
+
+    }
+    return self;
+}
+@end
